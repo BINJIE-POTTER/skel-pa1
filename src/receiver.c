b61e594ec8ecdbe46f7b6c2f4400cec7583a40b7
Binjie Ye <yebinjie@student.ubc.ca>
2024-03-02 14:21:30 -0800

    first commit

--- src/receiver.c
+++ src/receiver.c
@@ -11,24 +11,93 @@
 #include <pthread.h>
 #include <errno.h>
 
-void rrecv(unsigned short int myUDPport, 
-            char* destinationFile, 
-            unsigned long long int writeRate) {
+#define BUFFER_SIZE 1024 // Adjust based on expected MTU
+
+
+// void rrecv(unsigned short int myUDPport, 
+//             char* destinationFile, 
+//             unsigned long long int writeRate) {
+
+// }
+
+// int main(int argc, char** argv) {
+//     // This is a skeleton of a main function.
+//     // You should implement this function more completely
+//     // so that one can invoke the file transfer from the
+//     // command line.
+
+//     unsigned short int udpPort;
+
+//     if (argc != 3) {
+//         fprintf(stderr, "usage: %s UDP_port filename_to_write\n\n", argv[0]);
+//         exit(1);
+//     }
+
+//     udpPort = (unsigned short int) atoi(argv[1]);
+// }
+
+void rrecv(unsigned short int myUDPport, char* destinationFile, unsigned long long int writeRate) {
+
+    (void)writeRate;
+
+    int sockfd;
+    struct sockaddr_in servaddr, cliaddr;
+    char buffer[BUFFER_SIZE];
+    FILE *fp;
+
+    sockfd = socket(AF_INET, SOCK_DGRAM, 0);
+    if (sockfd < 0) {
+        perror("socket creation failed");
+        exit(EXIT_FAILURE);
+    }
+
+    memset(&servaddr, 0, sizeof(servaddr));
+    servaddr.sin_family = AF_INET;
+    servaddr.sin_addr.s_addr = INADDR_ANY;
+    servaddr.sin_port = htons(myUDPport);
+
+    if (bind(sockfd, (const struct sockaddr *)&servaddr, sizeof(servaddr)) < 0) {
+        perror("bind failed");
+        exit(EXIT_FAILURE);
+    }
+
+    fp = fopen(destinationFile, "wb");
+    if (fp == NULL) {
+        perror("Failed to open file");
+        exit(EXIT_FAILURE);
+    }
+
+    printf("Receiver is listening on port %hu\n", myUDPport);
+
+    socklen_t len = sizeof(cliaddr);
+    int n;
+    while ((n = recvfrom(sockfd, buffer, BUFFER_SIZE, 0, (struct sockaddr *) &cliaddr, &len)) > 0) {
+        fwrite(buffer, 1, n, fp);
+        printf("wrote...");
+    }
+
+    if (n < 0) {
+        perror("recvfrom failed");
+    }
+
+    fclose(fp);
+    close(sockfd);
+
+    printf("Receiver terminated\n");
 
 }
 
 int main(int argc, char** argv) {
-    // This is a skeleton of a main function.
-    // You should implement this function more completely
-    // so that one can invoke the file transfer from the
-    // command line.
-
-    unsigned short int udpPort;
-
-    if (argc != 3) {
-        fprintf(stderr, "usage: %s UDP_port filename_to_write\n\n", argv[0]);
-        exit(1);
+    if (argc != 4) {
+        fprintf(stderr, "Usage: %s <UDP port> <destination file> <write rate>\n", argv[0]);
+        return 1;
     }
 
-    udpPort = (unsigned short int) atoi(argv[1]);
+    unsigned short int udpPort = (unsigned short int)atoi(argv[1]);
+    char* destinationFile = argv[2];
+    unsigned long long int writeRate = atoll(argv[3]);
+
+    rrecv(udpPort, destinationFile, writeRate);
+
+    return 0;
 }
--- src/sender.c
+++ src/sender.c
@@ -11,30 +11,113 @@
 #include <pthread.h>
 #include <errno.h>
 
-void rsend(char* hostname, 
-            unsigned short int hostUDPport, 
-            char* filename, 
-            unsigned long long int bytesToTransfer) 
-{
+#define BUFFER_SIZE 65536 // Adjust based on expected MTU
 
+
+// void rsend(char* hostname, 
+//             unsigned short int hostUDPport, 
+//             char* filename, 
+//             unsigned long long int bytesToTransfer) 
+// {
+
+// }
+
+// int main(int argc, char** argv) {
+//     // This is a skeleton of a main function.
+//     // You should implement this function more completely
+//     // so that one can invoke the file transfer from the
+//     // command line.
+//     int hostUDPport;
+//     unsigned long long int bytesToTransfer;
+//     char* hostname = NULL;
+
+//     if (argc != 5) {
+//         fprintf(stderr, "usage: %s receiver_hostname receiver_port filename_to_xfer bytes_to_xfer\n\n", argv[0]);
+//         exit(1);
+//     }
+//     hostUDPport = (unsigned short int) atoi(argv[2]);
+//     hostname = argv[1];
+//     bytesToTransfer = atoll(argv[4]);
+
+//     return (EXIT_SUCCESS);
+// }
+
+void rsend(char* hostname, unsigned short int hostUDPport, char* filename, unsigned long long int bytesToTransfer) {
+    
+    int sockfd;
+    struct sockaddr_in servaddr;
+    FILE *fp;
+    char buffer[BUFFER_SIZE];
+
+    printf("Hostname: %s\n", hostname);
+    printf("UDP Port: %hu\n", hostUDPport);
+    printf("File to send: %s\n", filename);
+    printf("Bytes to transfer: %llu\n", bytesToTransfer);
+
+    sockfd = socket(AF_INET, SOCK_DGRAM, 0);
+    if (sockfd < 0) {
+        perror("socket creation failed");
+        exit(EXIT_FAILURE);
+    }
+
+    memset(&servaddr, 0, sizeof(servaddr));
+    servaddr.sin_family = AF_INET;
+    servaddr.sin_port = htons(hostUDPport);
+
+    int addrResult = inet_pton(AF_INET, hostname, &servaddr.sin_addr);
+    if (addrResult == 0) {
+        fprintf(stderr, "The address does not contain a valid IPv4 address string\n");
+        exit(EXIT_FAILURE);
+    } else if (addrResult < 0) {
+        perror("inet_pton failed due to an invalid address family or other error");
+        exit(EXIT_FAILURE);
+    }
+
+    printf("Trying to open the file...\n");
+
+    fp = fopen(filename, "rb");
+    if (fp == NULL) {
+        perror("Failed to open file");
+        exit(EXIT_FAILURE);
+    }
+
+    printf("Succeed to open the file!\n");
+
+    unsigned long long int sentBytes = 0;
+    while (sentBytes < bytesToTransfer && fread(buffer, 1, BUFFER_SIZE, fp) > 0) {
+        ssize_t toSend = (bytesToTransfer - sentBytes < BUFFER_SIZE) ? bytesToTransfer - sentBytes : BUFFER_SIZE;
+        ssize_t sent = sendto(sockfd, buffer, toSend, 0, (const struct sockaddr *) &servaddr, sizeof(servaddr));
+        if (sent < 0) {
+            perror("Failed to send file");
+            break;
+        } else {
+            printf("sent...\n");
+        }
+        sentBytes += sent;
+    }
+
+    printf("sentBytes: %llu\n", sentBytes);
+
+    fclose(fp);
+    close(sockfd);
+
+    printf("END\n");
 }
 
 int main(int argc, char** argv) {
-    // This is a skeleton of a main function.
-    // You should implement this function more completely
-    // so that one can invoke the file transfer from the
-    // command line.
-    int hostUDPport;
-    unsigned long long int bytesToTransfer;
-    char* hostname = NULL;
-
     if (argc != 5) {
-        fprintf(stderr, "usage: %s receiver_hostname receiver_port filename_to_xfer bytes_to_xfer\n\n", argv[0]);
-        exit(1);
+        fprintf(stderr, "Usage: %s <hostname> <UDP port> <file to send> <bytes to transfer>\n", argv[0]);
+        return 1;
     }
-    hostUDPport = (unsigned short int) atoi(argv[2]);
-    hostname = argv[1];
-    bytesToTransfer = atoll(argv[4]);
 
-    return (EXIT_SUCCESS);
+    char* hostname = argv[1];
+    unsigned short int hostUDPport = (unsigned short int)atoi(argv[2]);
+    char* filename = argv[3];
+    unsigned long long int bytesToTransfer = atoll(argv[4]);
+
+    rsend(hostname, hostUDPport, filename, bytesToTransfer);
+
+    printf("MAIN END\n");
+
+    return 0;
 }
